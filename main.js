7afdbd55322b5fedda0224e3914522550adec1cb
Abhyuday Tripathi <tripathi.abhyuday2010@gmail.com>
2021-05-25 13:38:10 +0530

    Add Explore Page.

---
+++ main.js
@@ -0,0 +1,43 @@
+let video;
+let detector;
+let detections = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  video = createCapture(VIDEO, videoReady);
+  video.size(640, 480);
+  video.hide();
+}
+
+function videoReady() {
+  detector = ml5.objectDetector('cocossd', modelReady);
+}
+
+function gotDetections(error, results) {
+  if (error) {
+    console.error(error);
+  }
+
+  detections = results;
+  detector.detect(video, gotDetections);
+}
+
+function modelReady() {
+  detector.detect(video, gotDetections);
+}
+
+function draw() {
+  image(video, 0, 0);
+
+  for (let i = 0; i < detections.length; i += 1) {
+    const object = detections[i];
+    stroke(0, 255, 0);
+    strokeWeight(4);
+    noFill();
+    rect(object.x, object.y, object.width, object.height);
+    noStroke();
+    fill(255);
+    textSize(24);
+    text(object.label, object.x + 10, object.y + 24);
+  }
+}
